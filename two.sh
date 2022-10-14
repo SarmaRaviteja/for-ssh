147db093e83c1d2c68d5b7081b76dcfba366649c
SarmaRaviteja <105492605+SarmaRaviteja@users.noreply.github.com>
2022-10-14 12:42:01 +0530

    Update two.sh

--- two.sh
+++ two.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-a=2
-b=1
-if [$a -ge $b]
-then 
-	echo "The variable 'a'is greater than the variable 'b'"
-else 
-	echo "The variable 'b'is greater than the variable 'a'"
-fi 
+#$a=2
+#b=1
+#if [$a -ge $b]
+#then 
+#	echo "The variable 'a'is greater than the variable 'b'"
+#else 
+#	echo "The variable 'b'is greater than the variable 'a'"
+#fi 
+
+
+
+echo " Hello Raviteja You are building a new pipeline " 
+echo " ############################################################################ "
+
+pwd
 
